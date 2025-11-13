f6a2f3e167616b040252c0b08ff14ffe70453809
IagoYuriRossan <iagorossan321@gmail.com>
2025-11-12 21:32:27 -0300

    feat: Adicionar telas de Deletar e Alteração ao navegador

--- Mobile I/Cadastro_Consulta/CADASTRO_CONSULTA/App.js
+++ Mobile I/Cadastro_Consulta/CADASTRO_CONSULTA/App.js
@@ -4,7 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CadastroScreen from './screens/CadastroScreen';
 import ConsultaScreen from './screens/ConsultaScreen';
-import HomeScreen from './screens/HomeScreen'; // Import da nova tela
+import HomeScreen from './screens/HomeScreen';
+import DeletarScreen from './screens/DeletarScreen';
+import AlteracaoScreen from './screens/AlteracaoScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +17,8 @@ const App = () => {
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Página Inicial' }} />
         <Stack.Screen name="Cadastro" component={CadastroScreen} />
         <Stack.Screen name="Consulta" component={ConsultaScreen} />
+        <Stack.Screen name="Deletar" component={DeletarScreen} />
+        <Stack.Screen name="Alteracao" component={AlteracaoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
