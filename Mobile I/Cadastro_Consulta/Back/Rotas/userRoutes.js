a8816e34e4aacbe70459bef4f7a6aecd62c940be
IagoYuriRossan <iagorossan321@gmail.com>
2025-11-12 21:41:24 -0300

    feat: Adicionar rotas para atualização e deleção de usuários com validação

--- Mobile I/Cadastro_Consulta/Back/Rotas/userRoutes.js
+++ Mobile I/Cadastro_Consulta/Back/Rotas/userRoutes.js
@@ -32,4 +32,40 @@ router.get('/consulta', (req, res) => {
   res.status(200).json(users); // Retornando o array de usuários
 });
 
+//Rota para atualizar um usuário pelo id
+router.put('/atualizar/:id', (req, res) => {
+  const { id } = req.params;
+  const { nome, email, senha } = req.body;
+
+  // Encontrar o usuário pelo id
+  const user = users.findIndex((user) => user.id === parseFloat.int(id));
+
+  //verificar se o usuário existe
+  if (user === -1) {
+    return res.status(404).json({ message: 'Usuário não encontrado.' });
+  }
+
+  //validar os dados
+  if (!nome && !email && !senha) {
+    return res.status(400).json({ message: 'Pelo menos um campo (nome, email ou senha) deve ser fornecido para atualização.' });
+  }
+
+  // Atualizar os dados do usuário
+  users[userIndex] = { id: parseInt(id), nome, email, senha };
+  res.status(200).json({ message: 'Usuário atualizado com sucesso.', user: users[user] });
+});
+
+// Rota para deletar um usuário pelo id
+router.delete('/deletar/:id', (req, res) => {
+  const { id } = req.params;
+  const userIndex = users.findIndex(user => user.id === id);
+  if (userIndex === -1) {
+    return res.status(404).json({ message: 'Usuário não encontrado.' });
+  }
+
+  // Remover o usuário do array
+  users.splice(userIndex, 1);
+  res.status(200).json({ message: 'Usuário deletado com sucesso.' });
+});
+
 module.exports = router;
